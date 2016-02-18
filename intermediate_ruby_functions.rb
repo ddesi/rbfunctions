22d44a91cbb46b53790f43abda3f83ccd9ff5cf0
desi <desi.dmt@gmail.com>
2016-02-18 17:08:07 -0500

    rotation and factorial

---
+++ intermediate_ruby_functions.rb
@@ -0,0 +1,47 @@
+# --------------------------------- R O T A T I O N ----------------------------------------------------------
+	# def rotate (array, n)
+	# 	if n > 0
+	# 		new_array = array.push + array.shift(n)
+	# 		puts new_array
+	# 	else
+	# 		y = (array.length + n)
+	# 		new_array = array.push + array.shift(y)
+	# 		puts new_array
+	# 	end
+	# end
+
+
+# array = [1, 2, 3, 4, 5]
+# the n is equal to 4
+
+# array = ["cat", "dog", "mouse", "shoe"]
+# the n is equal to 2
+
+# array = ["cat", "dog", "mouse", "shoe"]
+# the n is equal to -1
+
+# rotate(array, 4)
+
+
+
+# --------------------------------- F A C T O R I A L --------------------------------------------------------
+
+# Create an n factorial function. A factorial is the evaluation of n!
+# This number is calculated by multiplying every number from 1 to and including n
+
+# Edge cases: If your input is a negative integer Your output should be: undefined
+# If your input is: 0
+# Your output should be: 1
+
+# otherwise it should be output = number * (n-1)
+
+# def fact(number)
+#   if number == 0
+#     1
+#   else
+#     number * fact(number-1)
+#   end
+# end
+
+# puts fact(7)
+
